4b86fde21c65c00022663c625789cb5fe6e24547
Samuel Darenskiy <samdarenskiy@protonmail.com>
2022-01-30 23:32:27 +0600

    'added'

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ from app import database
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
-    database_password: str = ""
+    database_password: str
     database_name: str
     database_username: str
     secret_key: str

bb5eb1745cb1b7958ec640d45bbc0bfa9aefd693
Samuel Darenskiy <samdarenskiy@protonmail.com>
2022-02-02 12:21:12 +0600

    edits

--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
+    database_url: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
